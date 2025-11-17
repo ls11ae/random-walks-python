65726546c2969623d5f6f088656aa966bfd72789
Omar Chatila <omar.chatila@tu-dortmund.de>
2025-11-17 03:09:45 +0100

    feat: simplify JSON walk handling, update dependencies, and refine CMake install setup`

--- random_walk_package/bindings/data_processing/walk_json.py
+++ random_walk_package/bindings/data_processing/walk_json.py
@@ -18,20 +18,6 @@ dll.save_walk_to_json_noterrain.restype = None
 dll.save_walk_to_json_onlywalk.argtypes = [Point2DArrayPtr, c_size_t, c_size_t, c_char_p]
 dll.save_walk_to_json_onlywalk.restype = None
 
-# load from file
-dll.load_full_walk.argtypes = [c_char_p, Point2DArrayPtr, Point2DArrayPtr, TerrainMapPtr]
-dll.load_full_walk.restype = None
-
-dll.load_walk_with_terrain.argtypes = [c_char_p, Point2DArrayPtr, TerrainMapPtr]
-dll.load_walk_with_terrain.restype = None
-
-dll.load_walk_with_steps.argtypes = [c_char_p, Point2DArrayPtr, Point2DArrayPtr]
-dll.load_walk_with_steps.restype = None
-
-dll.load_walk_only.argtypes = [c_char_p, Point2DArrayPtr]
-dll.load_walk_only.restype = None
-
-
 def walk_to_json(walk, json_file: str, steps=None, terrain_map=None, W=None, H=None):
     file = c_char_p(json_file.encode('ascii'))
 
@@ -45,18 +31,3 @@ def walk_to_json(walk, json_file: str, steps=None, terrain_map=None, W=None, H=N
         dll.save_walk_to_json_onlywalk(walk, W, H, file)
 
     print("Walk saved to {}".format(json_file))
-
-
-def walk_from_json(json_file, steps=None, walk=None, terrain_map=None):
-    file = c_char_p(json_file.encode('ascii'))
-
-    if terrain_map is not None and steps is not None:
-        dll.load_full_walk(file, steps, steps, walk, terrain_map)
-    elif terrain_map is not None and steps is None:
-        dll.load_walk_with_terrain(file, walk, terrain_map)
-    elif terrain_map is None and steps is not None:
-        dll.load_walk_with_steps(file, steps, walk)
-    elif terrain_map is None and steps is None:
-        dll.load_walk_only(file, walk)
-
-    print("Walk loaded from {}".format(json_file))
